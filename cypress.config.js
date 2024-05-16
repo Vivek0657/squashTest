42a783b811d3cde9f104ebdc1e32d094b81a38cd
Vivek <vivekchaudhary657@gmail.com>
2024-05-16 15:53:21 +0530

    Pushing the latest changes to main

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ttyyrf",
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
